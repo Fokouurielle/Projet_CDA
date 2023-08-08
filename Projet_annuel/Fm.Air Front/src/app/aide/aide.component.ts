e59100594210f05932528919e63a870bfca61973
Fokouurielle <58878474+Fokouurielle@users.noreply.github.com>
2023-08-08 21:18:31 +0200

    component aide

---
+++ Projet_annuel/Fm.Air Front/src/app/aide/aide.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-aide',
+    templateUrl: './aide.component.html',
+    styleUrls: ['./aide.component.scss']
+})
+
+export class AideComponent implements OnInit {
+  focus: any;
+  focus1: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
